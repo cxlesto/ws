7ca60247e5f6ed56f152bd2c578e1433300bc62d
lascyrx <134105195+cxlesto@users.noreply.github.com>
2026-02-18 18:50:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const socket = io("wss://wss.celesto.io");
+
+socket.on("message", text => {
+
+  const el = document.createElement("li");
+  el.innerHTML = text;
+  document.querySelector("ul").appendChild(el);
+});
+
+document.querySelector("button").onclick = () => {
+  const text = document.querySelector("input").value;
+  socket.emit("message", text);
+}
